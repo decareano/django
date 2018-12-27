baeac8e13e66d02a68e8d04e2c9efec78fa721c9
Marcelo Gobelli <mgobelli@Mac-loaner02.local>
2018-12-26 19:07:17 -0800

    initial failing test

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,18 +1,7 @@
-# # encoding: utf-8
+require 'spec_helper'
 
-# Inspec test for recipe django::default
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe 'django::default' do
+	let(:command) {'django-admin --version'}
+	it { should match(/1.6.1/)}
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
-end
